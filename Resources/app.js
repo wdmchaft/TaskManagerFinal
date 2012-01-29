97698ffcf8e4dcf87e942592f12f52b803a3d035
mobiel <mobiel@cabaret.local>
2012-01-29 21:47:32 +0100

    added windows, switching logic

--- Resources/app.js
+++ Resources/app.js
@@ -4,18 +4,50 @@ Ti.include("/include/mvc.js");
 
 // controller includes
 Ti.include("/controllers/loginController.js");
+Ti.include("/controllers/taskController.js");
+Ti.include("/controllers/addTaskController.js");
 
 var loginWin = Ti.UI.createWindow({ exitOnClose : true });
 var loginController = new LoginController(loginWin);
 
+var taskWin = Ti.UI.createWindow({ exitOnClose : true });
+var taskController = new TaskController(taskWin);
+
+var addTaskView = Ti.UI.createWindow({ exitOnClose : true });
+var addTaskController = new AddTaskController(addTaskView);
+
+// a canvas window to add our navigation bar to
+var canvas = Ti.UI.createWindow({ exitOnClose : true });
+
+var nav = Titanium.UI.iPhone.createNavigationGroup({
+    window:taskWin
+});
+
+Ti.App.addEventListener("loggedIn", function() {
+	canvas.add(nav);
+	canvas.open();	
+	
+	taskController.view.loadTasks();
+});
 
 Ti.App.addEventListener("changeScreen", function(e)
 {
 	switch(e.changeTo)
 	{
-		case 'showTaskView':
-			Ti.API.info("showing task view");
-			//nav.open(taskView, { animated : true });
+		case 'showTasks':
+			taskController.view.loadTasks();
+			nav.open(taskWin, { animated : true });
+			break;
+		case 'addTask':
+			nav.open(addTaskView, { animated : true });
+			break;
+		case 'closeAddTask':
+			nav.close(addTaskView, { animated: true });
+			break;
+		case 'loginView':
+			nav.close();
+			canvas.close();
+			loginWin.open({ animated: true });
 			break;
 	}
 });
